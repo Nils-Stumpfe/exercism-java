ca91494d0b3ad2a7867a59ba4815d5e51753274a
Nils Stumpfe <nstumpfe@stud.hs-heilbronn.de>
2025-02-06 23:01:47 +0100

    nvm war doch besser so

--- annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
+++ annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -12,6 +12,6 @@ class AnnalynsInfiltration {
     }
 
     public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
-        return (petDogIsPresent) || (!knightIsAwake  && prisonerIsAwake) && !archerIsAwake;
+        return ((petDogIsPresent) || (!knightIsAwake  && prisonerIsAwake)) && !archerIsAwake;
     }
 }
