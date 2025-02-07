9e98c93a35d7ea427ba4d28eaeed08296c248323
Nils Stumpfe <nstumpfe@stud.hs-heilbronn.de>
2025-02-07 20:52:24 +0100

    inital calc implementation

--- calculator-conundrum/src/main/java/CalculatorConundrum.java
+++ calculator-conundrum/src/main/java/CalculatorConundrum.java
@@ -1,5 +1,20 @@
 class CalculatorConundrum {
+
     public String calculate(int operand1, int operand2, String operation) {
-        throw new UnsupportedOperationException("Please implement the CalculatorConundrum.calculate() method");
+        
+        switch (operation) {
+            case null : throw new IllegalArgumentException("Operation cannot be null");
+            case "" : throw new IllegalArgumentException("Operation cannot be empty");
+            case "+" : return operand1 + " + " + operand2 + " = " + (operand1 + operand2);
+            case "*" : return operand1 + " * " + operand2 + " = " + (operand1 * operand2);
+            case "/" : 
+                try {
+                return operand1 + " / " + operand2 + " = " + (operand1 / operand2);
+                } catch (ArithmeticException ae) {
+                    throw new IllegalOperationException("Division by zero is not allowed", ae);
+                }
+            default : throw new IllegalOperationException("Operation '" + operation + "' does not exist");
+        }
     }
+
 }
