cc9f6c22ddb1fbc29c96ba35a6d11b8ce6355f9e
Nils Stumpfe <nstumpfe@stud.hs-heilbronn.de>
2025-02-07 21:09:55 +0100

    forgor the copy 💀

--- calculator-conundrum/src/main/java/CalculatorConundrum.java
+++ calculator-conundrum/src/main/java/CalculatorConundrum.java
@@ -2,18 +2,29 @@ class CalculatorConundrum {
 
     public String calculate(int operand1, int operand2, String operation) {
         
-        switch (operation) {
-            case null : throw new IllegalArgumentException("Operation cannot be null");
-            case "" : throw new IllegalArgumentException("Operation cannot be empty");
-            case "+" : return operand1 + " + " + operand2 + " = " + (operand1 + operand2);
-            case "*" : return operand1 + " * " + operand2 + " = " + (operand1 * operand2);
-            case "/" : 
-                try {
-                return operand1 + " / " + operand2 + " = " + (operand1 / operand2);
-                } catch (ArithmeticException ae) {
-                    throw new IllegalOperationException("Division by zero is not allowed", ae);
-                }
-            default : throw new IllegalOperationException("Operation '" + operation + "' does not exist");
+        int result =
+            switch (operation) {
+                case "+" -> operand1 + operand2;
+                case "*" -> operand1 * operand2;
+                case "/" -> divide(operand1, operand2);
+
+                case "" -> throw new IllegalArgumentException("Operation cannot be empty");
+                case null -> throw new IllegalArgumentException("Operation cannot be null");
+                default -> throw new IllegalOperationException("Operation '" + operation + "' does not exist");
+            };
+
+        return String.format("%s %s %s = %s", operand1, operation, operand2, result);
+    }
+
+    // inspired by florian-zeidler and sharmaashish54
+    //https://exercism.org/tracks/java/exercises/calculator-conundrum/solutions/florian-zeidler
+    //https://exercism.org/tracks/java/exercises/calculator-conundrum/solutions/sharmaashish54
+
+    private int divide(int operand1, int operand2) {
+        try {
+            return operand1 / operand2;
+        } catch (ArithmeticException ae) {
+            throw new IllegalOperationException("Division by zero is not allowed", ae);
         }
     }
 
