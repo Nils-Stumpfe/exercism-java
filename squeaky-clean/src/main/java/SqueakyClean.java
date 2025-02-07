fbcd1a723867055fb1bcc33c9d8d953093aebea0
Nils Stumpfe <nstumpfe@stud.hs-heilbronn.de>
2025-02-07 23:11:13 +0100

    shloud have been more commits tbh. inital submission

--- squeaky-clean/src/main/java/SqueakyClean.java
+++ squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,5 +1,49 @@
+import static java.lang.Character.*;
+
 class SqueakyClean {
-    static String clean(String identifier) {
-        throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
+
+    static String clean(String text) {
+        return convertBlanksToUnderscore(
+                                        omitNonAlphaNumericCharacter(
+                                        convertLeetSpeak(
+                                        convertKebabCaseToCamelCase(text))));
+    }
+
+    private static String convertBlanksToUnderscore (String text) {
+        return text.replace(' ', '_');
+    }
+
+    private static String convertKebabCaseToCamelCase (String text) {
+        if (text.contains("-")) {
+
+        String[] stringPart = text.toLowerCase().split("-");
+        String returnString = stringPart[0];    
+        for (int i = 1; i < stringPart.length; i++) {
+            char[] character = stringPart[i].toCharArray();
+            character[0] = Character.toUpperCase(character[0]);
+            stringPart[i] = new String(character);
+            returnString = returnString.concat(stringPart[i]);
+        }
+        return returnString;
+        }
+
+        return text;
+    }
+
+    private static String convertLeetSpeak (String text) {
+        return text.replace('4', 'a')
+                    .replace('3', 'e')
+                    .replace('0', 'o')
+                    .replace('1', 'l')
+                    .replace('7', 't');
+
+    }
+
+    private static String omitNonAlphaNumericCharacter (String text) {
+        char[] character = text.toCharArray();   
+        for (int i = 0; i < character.length; i++) {
+            character[i] = (isLetter(character[i]) || character[i] == ' ') ? character[i] : '#';
+        }
+        return new String(character).replace("#", "");
     }
 }
