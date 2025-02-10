a763b77381f95217114484df86f6df66d4a8add1
Nils Stumpfe <nstumpfe@stud.hs-heilbronn.de>
2025-02-10 14:56:04 +0100

    initial salary-cals impl

--- salary-calculator/src/main/java/SalaryCalculator.java
+++ salary-calculator/src/main/java/SalaryCalculator.java
@@ -1,17 +1,21 @@
 public class SalaryCalculator {
-    public double salaryMultiplier(int daysSkipped) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.salaryMultiplier() method");
-    }
 
+    private static final double BASE_SALARY = 1000.0;
+
+    public double salaryMultiplier(int daysSkipped) {
+       return (daysSkipped < 5) ? 1 : 0.85;
+    }
+    
     public int bonusMultiplier(int productsSold) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.bonusMultiplier() method");
+        return (productsSold >= 20) ? 13 : 10;
     }
 
     public double bonusForProductsSold(int productsSold) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.bonusForProductsSold() method");
+       return productsSold * bonusMultiplier(productsSold);
     }
 
     public double finalSalary(int daysSkipped, int productsSold) {
-        throw new UnsupportedOperationException("Please implement the SalaryCalculator.finalSalary() method");
+        double salary = BASE_SALARY * salaryMultiplier(daysSkipped) + bonusForProductsSold(productsSold); 
+        return (salary > 2000.0) ? 2000.0 : salary;
     } 
 }
