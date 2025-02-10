e67402c356482b8453cd943d96f7a4bff8c2db1f
Nils Stumpfe <nstumpfe@stud.hs-heilbronn.de>
2025-02-10 14:00:15 +0100

    initial caass impl

--- cars-assemble/src/main/java/CarsAssemble.java
+++ cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,17 @@
 public class CarsAssemble {
 
+    private static final int PRODUCTION_RATE = 221;
+    private static final int[] SUCCES_RATE= {0, 100, 100, 100, 100, 90, 90, 90, 90, 80, 77};
+
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRatePerHour() method");
+        return (speed * PRODUCTION_RATE) * getSuccesRate(speed);
+    }
+
+    public double getSuccesRate(int speed) {
+        return (double) SUCCES_RATE[speed] / 100;
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+        return (int) (productionRatePerHour(speed) / 60);
     }
 }
