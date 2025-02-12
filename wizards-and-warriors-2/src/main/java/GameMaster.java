39dbc0b3c40321a7e7b6430362ca5a52320ee908
Nils Stumpfe <nstumpfe@stud.hs-heilbronn.de>
2025-02-12 22:01:38 +0100

    initial impl

--- wizards-and-warriors-2/src/main/java/GameMaster.java
+++ wizards-and-warriors-2/src/main/java/GameMaster.java
@@ -1,12 +1,25 @@
 public class GameMaster {
 
-    // TODO: define a 'describe' method that returns a description of a Character
+    public String describe (Character character) {
+        return String.format("You're a level %s %s with %s hit points.", character.getLevel(), character.getCharacterClass(), character.getHitPoints());
+    }
 
-    // TODO: define a 'describe' method that returns a description of a Destination
+    public String describe (Destination destination) {
+        return String.format("You've arrived at %s, which has %s inhabitants.",destination.getName(), destination.getInhabitants());
+    }
 
-    // TODO: define a 'describe' method that returns a description of a TravelMethod
+    public String describe (TravelMethod travelMethod) {
+        return switch (travelMethod) {
+            case WALKING -> "You're traveling to your destination by walking.";
+            case HORSEBACK -> "You're traveling to your destination on horseback.";
+        };
+    }
 
-    // TODO: define a 'describe' method that returns a description of a Character, Destination and TravelMethod
+    public String describe (Character character, Destination destination, TravelMethod travelMethod) {
+        return this.describe(character) + " " +  this.describe(travelMethod) + " " + this.describe(destination);
+    }
 
-    // TODO: define a 'describe' method that returns a description of a Character and Destination
+    public String describe (Character character, Destination destination) {
+        return this.describe(character, destination, TravelMethod.WALKING);
+    }
 }
