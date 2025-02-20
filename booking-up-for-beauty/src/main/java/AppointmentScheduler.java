b22b3dd5abe7837a24e33cb506d1665658f97895
Nils Stumpfe <nstumpfe@stud.hs-heilbronn.de>
2025-02-20 16:58:46 +0100

    schedule & Description müssen überarbeitet werden.

--- booking-up-for-beauty/src/main/java/AppointmentScheduler.java
+++ booking-up-for-beauty/src/main/java/AppointmentScheduler.java
@@ -2,23 +2,37 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 class AppointmentScheduler {
+
+    private LocalDate since = LocalDate.of(2012, 9, 15);
+
     public LocalDateTime schedule(String appointmentDateDescription) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.schedule() method");
+        return LocalDateTime.parse(appointmentDateDescription);
     }
 
     public boolean hasPassed(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.hasPassed() method");
+      return appointmentDate.isBefore(LocalDateTime.now());
     }
 
     public boolean isAfternoonAppointment(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.isAfternoonAppointment() method");
+        return (appointmentDate.getHour() >= 12 && appointmentDate.getHour() < 18) ? true : false;
     }
 
+    // Formatter benutzen.
     public String getDescription(LocalDateTime appointmentDate) {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.getDescription() method");
+        return String.format("You have an appointment on %s, %s %s, %s, at %s:00 %s.", 
+                                                                appointmentDate.getDayOfWeek(),
+                                                                appointmentDate.getMonth(),
+                                                                appointmentDate.getDayOfMonth(),
+                                                                appointmentDate.getYear(),
+                                                                (appointmentDate.getHour() > 12) ? appointmentDate.getHour()-12 : appointmentDate.getHour(),
+                                                                (appointmentDate.getHour() > 12) ? "PM" : "AM");
     }
 
     public LocalDate getAnniversaryDate() {
-        throw new UnsupportedOperationException("Please implement the AppointmentScheduler.getAnniversaryDate() method");
+        return LocalDate.of(LocalDate.now().getYear(), since.getMonthValue(), since.getDayOfMonth());
+    }
+
+    public static void main(String[] args) {
+        LocalDateTime datetime = LocalDateTime.of(2007, 12, 3, 22, 15, 30);
     }
 }
